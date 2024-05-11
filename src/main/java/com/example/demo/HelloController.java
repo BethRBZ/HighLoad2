9ab2c1d7c4a7c16f91b571362173e5d9ac264769
BethRBZ <zeliza8kl@gmail.com>
2024-05-11 16:27:20 +0500

    3

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,16 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.JedisPooled;
 
 @RestController
 public class HelloController {
     private final static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+    JedisPooled jedis = new JedisPooled("localhost", 6379);
 
     int counter = 0;
         @GetMapping("/hello")
         int hello() {
             ++counter;
-            LOGGER.info("Counter: {}", counter);
+            String count = Integer.toString(counter);
+            jedis.set("Counter", count);
+            LOGGER.info("Counter: {}", jedis.get("Counter"));
             return counter;
         }
 }
